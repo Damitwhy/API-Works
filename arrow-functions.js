2c3084af995a8636abdeab1dd52ade016cc08fa7
Ken Cox <159484600+Damitwhy@users.noreply.github.com>
2024-06-22 04:43:41 +0100

    added functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,41 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+console.log(addTwoNumbers(3, 5));
+
 // Arrow Function With Parameters
+
+const addTwoNumbers1 = (a, b) => {
+    return a + b;
+}
+console.log(addTwoNumbers1(3, 5));
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+console.log(addTwoNumbers2(3, 5));
+
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
+
+// Returning Objects
+
+const returnPerson = () => ({name: 'Adam'});
+console.log(returnPerson());
+
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+);
+console.log(returnMultipleLines());
+
